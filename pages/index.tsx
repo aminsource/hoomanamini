b5a481f6c3fd0487d49fcf48b392968bf1de8382
Hooman Amini <amini.hooman@gmail.com>
2020-12-18 15:42:12 +0330

    change head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <Layout title="مانا">
     <Hero />
     <Services />
     <Featured />
