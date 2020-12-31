ea9d4638377d6477cca79d8b809da664bc8ff892
Hooman Amini <amini.hooman@gmail.com>
2020-12-31 03:43:23 +0330

    refactor

---
+++ components/Footer.js
@@ -0,0 +1,150 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="relative bg-gray-300 pt-8 pb-6">
+      <div
+        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+        style={{ height: '80px', transform: 'translateZ(0)' }}
+      >
+        <svg
+          className="absolute bottom-0 overflow-hidden"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+          <polygon
+            className="text-gray-300 fill-current"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-6/12 px-4">
+            <h4 className="text-3xl font-semibold">تماس</h4>
+            <h5 className="text-lg mt-3 mb-2 text-gray-700">
+              از روش های زیر می توانید با ما در تماس باشید.
+            </h5>
+            <div className="mt-6">
+              <svg className="h-5 w-5" viewBox="0 0 24 24">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+                </g>
+              </svg>
+
+              <svg className="h-5 w-5" viewBox="0 0 24 24">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                </g>
+              </svg>
+              <svg className="h-5 w-5" viewBox="0 0 24 24">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M22.54 6.42a2.78 2.78 0 00-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 00-1.94 2A29 29 0 001 11.75a29 29 0 00.46 5.33A2.78 2.78 0 003.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 001.94-2 29 29 0 00.46-5.25 29 29 0 00-.46-5.33z" />
+                  <path d="M9.75 15.02l5.75-3.27-5.75-3.27v6.54z" />
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div className="w-full lg:w-6/12 px-4">
+            <div className="flex flex-wrap items-top mb-6">
+              <div className="w-full lg:w-4/12 px-4 ml-auto">
+                <span className="block uppercase text-gray-600 text-sm font-semibold mb-2 mt-5">
+                  لینک های مفید
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                      href=""
+                    >
+                      نکست جی اس
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                      href=""
+                    >
+                      ری‌اکت
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                      href=""
+                    >
+                      تیل ویند
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                      href=""
+                    >
+                      گیت هاب
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="w-full lg:w-4/12 px-4 mt-5">
+                <span className="block uppercase text-gray-600 text-sm font-semibold mb-2">
+                  منابع دیگر
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                      href=""
+                    >
+                      مجوز MIT
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                      href=""
+                    >
+                      شرایط و ضوابط
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-400" />
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <div className="text-sm text-gray-600 font-semibold py-1">
+              کپی رایت © ۱۳۹۹ .
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import { AppProps } from "next/app";
-import "../styles/index.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
