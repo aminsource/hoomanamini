17aa83dff274bb52ce7e1c0a15f8d02c9653a109
Hooman Amini <amini.hooman@gmail.com>
2020-12-31 04:21:19 +0330

    house keeping

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -25,16 +25,14 @@ export default function Newsletter({ newsletters }) {
       />
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          Newsletter
+          خبرنامه
         </h1>
         <p className="text-gray-600 dark:text-gray-400 mb-4">
-          My newsletter provides a behind-the-scenes look into what I'm working
-          on and writing about. I frequently share some of my favorite articles
-          I've read, as well as anything fascinating about technology.
+          خبرنامه های فعالیت ها
         </p>
         <Subscribe />
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
-          Archive
+          آرشیو
         </h3>
         <div className="prose dark:prose-dark">
           <ul>
