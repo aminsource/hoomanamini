af9baa6a513d4a60d468b47e8e02d809e5486450
h.amini <h.amini@pep.co.ir>
2021-03-03 15:02:05 +0330

    edit intro redux post

--- layouts/blog.js
+++ layouts/blog.js
@@ -7,10 +7,10 @@ import ViewCounter from '@/components/ViewCounter';
 import BlogSeo from '@/components/BlogSeo';
 
 const editUrl = (slug) =>
-  `https://github.com/leerob/manakit.ir/edit/master/data/blog/${slug}.mdx`;
+  `https://github.com/aminsource/hoomanamini/tree/master/data/blog/${slug}.mdx`;
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
-    `https://manakit.ir/blog/${slug}`
+    `https://hoomanamini.ir/blog/${slug}`
   )}`;
 
 export default function BlogLayout({ children, frontMatter }) {
@@ -21,7 +21,7 @@ export default function BlogLayout({ children, frontMatter }) {
         {...frontMatter}
       />
       <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full px-8">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+        <h1 className="text-1xl md:text-2xl tracking-tight mb-4 text-black dark:text-white">
           {frontMatter.title}
         </h1>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2 mb-8">
@@ -51,15 +51,15 @@ export default function BlogLayout({ children, frontMatter }) {
         {/* <div className="mt-8">
           <Subscribe />
         </div> */}
-        <div className="text-sm text-gray-700 dark:text-gray-300">
-          <a
+        <div className="text-sm text-blue-600 dark:text-gray-300 mt-5">
+          {/* <a
             href={discussUrl(frontMatter.slug)}
             target="_blank"
             rel="noopener noreferrer"
           >
             {'در توییتر بحث کنید'}
           </a>
-          {` • `}
+          {` • `} */}
           <a
             href={editUrl(frontMatter.slug)}
             target="_blank"
